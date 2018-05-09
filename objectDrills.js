029d4de6aa2b6cb89e30da11b5bd61083c9401c2
Albert Sare <thesundaymailman@gmail.com>
2018-05-09 08:54:01 -0700

    finished excercise 2

--- objectDrills.js
+++ objectDrills.js
@@ -1,15 +1,28 @@
 'use strict';
 
-//Object initializers and methods
+// //Object initializers and methods
 
-const loaf = {
-  flour: 300,
-  water: 210,
-  hydration: function () {
-    return (this.water / this.flour) * 100;
-  },
+// const loaf = {
+//   flour: 300,
+//   water: 210,
+//   hydration: function () {
+//     return (this.water / this.flour) * 100;
+//   },
+// };
+
+// console.log('Flour: ' + loaf.flour);
+// console.log('Water: ' + loaf.water);
+// console.log('Hydration: ' + loaf.hydration());
+
+// Iterating over an object's properties
+const someObject = {
+  foo: 'foo',
+  bar: 'bar',
+  fum: 'fum',
+  quux: 'quux',
+  spam: 'spam'
 };
 
-console.log('Flour: ' + loaf.flour);
-console.log('Water: ' + loaf.water);
-console.log('Hydration: ' + loaf.hydration());
+for (let key in someObject) {
+  console.log(key);
+}
